bdaa694c36ea4993f086b59c49c289ac99d29b0e
Chris Nethercott <ccnethercott@gmail.com>
2017-06-30 09:44:16 +0100

    Added the class with voids and variables

--- src/F1Lights/F1Lights.h
+++ src/F1Lights/F1Lights.h
@@ -2,4 +2,14 @@
 #define F1Lights_h
 
 #include "Arduino.h"
+
+class F1Lights {
+  public:
+    F1Lights(int pins[]);
+    void runSequence();
+    void lightsOff();
+  private:
+    int _pins[];
+}
+
 #endif
